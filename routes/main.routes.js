e41deaec8b1a49f5090c650db8124706cb0845ae
Manoj Jadhav <manoj.jadhav@neosoftmail.com>
2023-05-09 11:24:54 +0530

    initial commit

---
+++ routes/main.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { useCheerio, usePuppeteer, useAmazon, useScrapIphone } from "../controllers/main.controller.js";
+
+const router = Router();
+
+router.get('/cheerio', useCheerio);
+router.get('/puppeteer', usePuppeteer);
+router.get('/amazon', useAmazon);
+router.get('/amazon/iphone', useScrapIphone)
+
+export default router;
